e1572a9977c8e378cd58c265bbdcac11a2cd3d18
Rin <dwnssb@gmail.com>
2024-04-09 15:05:08 +0200

    feat: use TypeOrmModule in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,34 @@
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from './entities/user.entity'
+import { SpeciesLocation } from './entities/speciesLocation.entity'
+import { Species } from './entities/species.entity'
+import { Message } from './entities/message.entity'
+import { Location } from './entities/location.entity'
+import { Level } from './entities/level.entity'
+import { Conversation } from './entities/conversation.entity'
+import { Category } from './entities/category.entity'
+import { Catch } from './entities/catch.entity'
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        entities: [User, SpeciesLocation, Species, Message, Location, Level, Conversation, Category, Catch],
+        synchronize: true,
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
