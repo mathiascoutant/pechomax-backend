f9c9628fc481d3495a77b2bd5125401f1feabe04
Rin <dwnssb@gmail.com>
2024-04-15 14:39:54 +0200

    feat: enable Cors & validation pipe

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify
 import { AppModule } from './app.module'
 import fastifyCookie from '@fastify/cookie'
 import { ConfigService } from '@nestjs/config'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
   const config = app.get(ConfigService)
+
+  app.enableCors()
+  app.useGlobalPipes(new ValidationPipe())
   app.register(fastifyCookie, {
     secret: config.get('COOKIE_SECRET'),
   })
