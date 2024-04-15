77fd2a1fd65c1597bdbe47053f55601038307690
Rin <dwnssb@gmail.com>
2024-04-15 11:29:14 +0200

    feat: create auth/register route

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -14,4 +14,23 @@ export class UsersController {
     private readonly authService: AuthService
   ) {}
 
+  @Post('auth/register')
+  async signup(@Body() createUserDto: CreateUserDto, @Res() res: FastifyReply) {
+    const register = await this.authService.register(createUserDto)
+
+    if (register.isErr()) {
+      const { error } = register
+
+      if (error.type === 'UserAlreadyExistException') {
+        return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send('This user already exist')
+      }
+
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send('Internal server error')
+    }
+
+    res.setCookie('access_token', register.content.access_token)
+
+    return res.status(HttpStatus.CREATED).send(register.content.user)
+  }
+
 }
