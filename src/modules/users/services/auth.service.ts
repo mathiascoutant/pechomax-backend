1a91f03ea5309039f2bb9092d2709d3838484c81
Rin <dwnssb@gmail.com>
2024-04-15 10:11:35 +0200

    feat: create auth service

--- src/modules/users/services/auth.service.ts
+++ src/modules/users/services/auth.service.ts
@@ -1,27 +1,29 @@
-// import { Injectable } from '@nestjs/common'
-// import { JwtService } from '@nestjs/jwt'
-// import { UsersService } from './users.service'
-// import { compare, genSalt, hash } from 'bcrypt'
-// import { Option, Err, Ok } from '../../types/option'
-// import { Payload } from '../../types/payload'
-// import { CleanUser } from '../types/utility'
-// import { CreateUserDto } from '../dto/signup.dto'
-// import { BaseError } from '../../types/error'
-// import { UserAlreadyExistException, UserNotFoundException, WrongPasswordException } from '../types/error'
+import { Injectable } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { UsersService } from './users.service'
+import { compare, genSalt, hash } from 'bcrypt'
+import { Option, Err, Ok } from 'src/types/option'
+import { Payload } from 'src/types/payload'
+import { CleanUser } from '../types/utilities'
+import { CreateUserDto } from '../dto/create-user.dto'
+import { BaseError } from 'src/types/error'
+import { UserAlreadyExistException, UserNotFoundException, WrongPasswordException } from '../types/error'
+import { ConfigService } from '@nestjs/config'
 
-// @Injectable()
-// export class AuthService {
-//   constructor(
-//     private readonly userService: UsersService,
-//     private readonly jwtService: JwtService
-//   ) {}
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UsersService,
+    private readonly jwtService: JwtService,
+    private configService: ConfigService
+  ) {}
 
-//   async hashPassword(password: string): Promise<string> {
-//     const salt = await genSalt()
-//     const hashedPassword = await hash(password, salt)
+  async hashPassword(password: string): Promise<string> {
+    const salt = await genSalt()
+    const hashedPassword = await hash(password, salt)
 
-//     return hashedPassword
-//   }
+    return hashedPassword
+  }
 
 //   async signup({
 //     email,
@@ -62,18 +64,4 @@
 
 //     const isMatch = await compare(password, user.content.password)
 
-//     if (isMatch) {
-//       const payload: Payload = {
-//         id: user.content.id,
-//         role: user.content.role,
-//       }
-//       return Ok({
-//         access_token: this.jwtService.sign(payload, {
-//           secret: process.env.JWT_SECRET,
-//         }),
-//       })
-//     } else {
-//       return Err(new WrongPasswordException())
-//     }
-//   }
-// }
+}
