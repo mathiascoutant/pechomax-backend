13012632de44ee1e5d87b8beab943a5c1916e3a0
Rin <dwnssb@gmail.com>
2024-04-15 11:25:35 +0200

    feat: register fastifyCookie

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
 import { AppModule } from './app.module'
+import fastifyCookie from '@fastify/cookie'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
+  app.register(fastifyCookie, {
+    secret: process.env.COOKIE_SECRET,
+  })
   await app.listen(3000)
 }
 bootstrap()
