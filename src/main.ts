2bd4819b16053a4df728d75422f6665d72e779b6
Rin <dwnssb@gmail.com>
2024-04-15 12:14:06 +0200

    fix: use configService in bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core'
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
 import { AppModule } from './app.module'
 import fastifyCookie from '@fastify/cookie'
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
+  const config = app.get(ConfigService)
   app.register(fastifyCookie, {
-    secret: process.env.COOKIE_SECRET,
+    secret: config.get('COOKIE_SECRET'),
   })
-  await app.listen(3000)
+
+  await app.listen(3000, '0.0.0.0')
 }
 bootstrap()
