86e46109e3be45be05da8943648aa482e5561370
Rin <dwnssb@gmail.com>
2024-04-17 15:44:59 +0200

    feat: env variables validation

---
+++ src/helpers/env.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod'
+
+const envSchema = z.object({
+  DB_HOST: z.string(),
+  DB_PORT: z.string(),
+  POSTGRES_USER: z.string(),
+  POSTGRES_PASSWORD: z.string(),
+  POSTGRES_DB: z.string(),
+  COOKIE_SECRET: z.string(),
+  JWT_SECRET: z.string(),
+  CORS_ORIGIN: z.string(),
+})
+
+envSchema.parse(process.env)
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace NodeJS {
+    interface ProcessEnv extends z.infer<typeof envSchema> {}
+  }
+}
