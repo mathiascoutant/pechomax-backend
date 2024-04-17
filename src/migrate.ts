6d11cf4a630e2c7e7265a29e3fec62f44cbe9121
Rin <dwnssb@gmail.com>
2024-04-17 15:45:14 +0200

    feat: create migration:run script

---
+++ src/migrate.ts
@@ -0,0 +1,20 @@
+import './helpers/env'
+import { drizzle } from 'drizzle-orm/node-postgres'
+import { migrate } from 'drizzle-orm/node-postgres/migrator'
+import pkg from 'pg'
+const { Client } = pkg
+
+const client = new Client({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+})
+
+await client.connect()
+const db = drizzle(client)
+
+await migrate(db, { migrationsFolder: 'src/drizzle/' })
+
+process.exit(0)
