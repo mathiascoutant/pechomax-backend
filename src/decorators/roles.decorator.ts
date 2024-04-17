1bd181f0f7574898d3cc1a589547f1ac00670859
Rin <dwnssb@gmail.com>
2024-04-17 15:42:24 +0200

    chore: remove nestjs files

--- src/decorators/roles.decorator.ts
+++
@@ -1,4 +0,0 @@
-import { SetMetadata } from '@nestjs/common'
-import { UserRoles } from 'src/modules/users/types/utilities'
-
-export const Roles = (...args: Array<UserRoles>) => SetMetadata('roles', args)
--- src/modules/users/types/utilities.ts
+++
@@ -1,5 +0,0 @@
-import { User } from 'src/entities/user.entity'
-
-export type UserRoles = typeof User.prototype.role
-
-export type CleanUser = Omit<User, 'password'>
