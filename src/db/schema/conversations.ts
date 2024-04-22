201550be45f395fbc6833abf01fba3d58c773812
Rin <dwnssb@gmail.com>
2024-04-22 16:41:16 +0200

    feat: create conversations relations

--- src/db/schema/conversations.ts
+++ src/db/schema/conversations.ts
@@ -1,6 +1,8 @@
 import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
 import { categories } from './categories'
 import { users } from './users'
+import { relations } from 'drizzle-orm'
+import { messages } from './messages'
 
 export const conversations = pgTable('conversations', {
   id: uuid('id').defaultRandom().primaryKey(),
@@ -15,6 +17,18 @@ export const conversations = pgTable('conversations', {
     .defaultNow()
     .$onUpdate(() => new Date()),
 })
+
+export const conversationsRelations = relations(conversations, ({ one, many }) => ({
+  user: one(users, {
+    fields: [conversations.userId],
+    references: [users.id],
+  }),
+  category: one(categories, {
+    fields: [conversations.categoryId],
+    references: [categories.id],
+  }),
+  messages: many(messages),
+}))
 
 export type Conversation = typeof conversations.$inferSelect
 export type NewConversation = typeof conversations.$inferInsert
