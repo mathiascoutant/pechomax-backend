8f0244c89ba14459a9c45afb2995a79189665a46
Rin <dwnssb@gmail.com>
2024-04-22 16:41:07 +0200

    feat: create categories relations

--- src/db/schema/categories.ts
+++ src/db/schema/categories.ts
@@ -1,9 +1,15 @@
+import { relations } from 'drizzle-orm'
 import { pgTable, text, uuid } from 'drizzle-orm/pg-core'
+import { conversations } from './conversations'
 
 export const categories = pgTable('categories', {
   id: uuid('id').defaultRandom().primaryKey(),
   name: text('name').unique().notNull(),
 })
+
+export const categoriesRelations = relations(categories, ({ many }) => ({
+  conversations: many(conversations),
+}))
 
 export type Category = typeof categories.$inferSelect
 export type NewCategory = typeof categories.$inferInsert
