dfb1e0f37918971a57c6d47061b7df1bcbee1350
Rin <dwnssb@gmail.com>
2024-04-22 09:44:41 +0200

    fix: conversations schema

--- src/db/schema/conversations.ts
+++ src/db/schema/conversations.ts
@@ -1,10 +1,14 @@
 import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
 import { categories } from './categories'
+import { users } from './users'
 
 export const conversations = pgTable('conversations', {
   id: uuid('id').defaultRandom().primaryKey(),
   title: text('title').notNull(),
-  categoryId: uuid('category_id').references(() => categories.id),
+  categoryId: uuid('category_id').references(() => categories.id, { onDelete: 'cascade' }),
+  userId: uuid('user_id')
+    .notNull()
+    .references(() => users.id),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at')
     .notNull()
