b6175df11d33f7f3802958c7239b0a5a6f02ae7e
Rin <dwnssb@gmail.com>
2024-04-22 16:40:47 +0200

    feat: create users relations

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -1,4 +1,10 @@
+import { relations } from 'drizzle-orm'
 import { integer, pgEnum, pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
+import { levels } from './levels'
+import { conversations } from './conversations'
+import { messages } from './messages'
+import { locations } from './locations'
+import { catches } from './catches'
 
 export const userRolesEnum = pgEnum('user_roles', ['Admin', 'User'])
 
@@ -13,6 +19,7 @@ export const users = pgTable('users', {
   city: text('city'),
   region: text('region'),
   zipCode: text('zip_code'),
+  levelId: uuid('level_id').references(() => levels.id),
   score: integer('score').default(0),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at')
@@ -20,6 +27,17 @@ export const users = pgTable('users', {
     .defaultNow()
     .$onUpdate(() => new Date()),
 })
+
+export const usersRelations = relations(users, ({ one, many }) => ({
+  level: one(levels, {
+    fields: [users.levelId],
+    references: [levels.id],
+  }),
+  conversations: many(conversations),
+  messages: many(messages),
+  locations: many(locations),
+  catches: many(catches),
+}))
 
 export type User = typeof users.$inferSelect
 export type NewUser = typeof users.$inferInsert
