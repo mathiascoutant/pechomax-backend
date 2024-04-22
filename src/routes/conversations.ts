8bccf9951efdeba6d2de06a47df72fb56e139e01
Rin <dwnssb@gmail.com>
2024-04-22 09:46:07 +0200

    feat: conversations get routes

--- src/routes/conversations.ts
+++ src/routes/conversations.ts
@@ -1,5 +1,42 @@
+import { zValidator } from '@hono/zod-validator'
+import { and, eq } from 'drizzle-orm'
 import { HonoVar } from 'src/helpers/hono'
+import { isAuth } from 'src/middlewares/isAuth'
+import { z } from 'zod'
 
 const conversationsRoute = new HonoVar().basePath('/conversations')
+
+conversationsRoute.get('/', async (ctx) => {
+  const db = ctx.get('database')
+
+  const conversations = await db.query.conversations.findMany()
+
+  ctx.json(conversations)
+})
+
+conversationsRoute.get(
+  '/:id',
+  zValidator(
+    'param',
+    z.object({
+      id: z.string(),
+    })
+  ),
+  async (ctx) => {
+    const db = ctx.get('database')
+    const { id } = ctx.req.valid('param')
+
+    const conversation = await db.query.conversations.findFirst({
+      where: (conv, { eq }) => eq(conv.id, id),
+    })
+
+    if (!conversation) {
+      return ctx.json({ message: 'Conversation not found' }, 404)
+    }
+
+    ctx.json(conversation)
+  }
+)
+
 
 export default conversationsRoute
