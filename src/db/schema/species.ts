0c90a701a6b9e247ac9b692ebdb638aa9fe6fc75
Rin <dwnssb@gmail.com>
2024-04-22 16:41:43 +0200

    feat: create species relations

--- src/db/schema/species.ts
+++ src/db/schema/species.ts
@@ -1,10 +1,18 @@
+import { relations } from 'drizzle-orm'
 import { integer, pgTable, text, uuid } from 'drizzle-orm/pg-core'
+import { catches } from './catches'
+import { speciesLocation } from './speciesLocation'
 
 export const species = pgTable('species', {
   id: uuid('id').defaultRandom().primaryKey(),
   name: text('name').unique(),
   pointValue: integer('point_value'),
 })
+
+export const speciesRlations = relations(species, ({ many }) => ({
+  catches: many(catches),
+  speciesLoactions: many(speciesLocation),
+}))
 
 export type Species = typeof species.$inferSelect
 export type NewSpecies = typeof species.$inferInsert
