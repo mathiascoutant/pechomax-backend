239d9d2ed44f8d9097c03b74387f6d0fcb81f488
Rin <dwnssb@gmail.com>
2024-04-22 09:46:33 +0200

    feat: conversations create route

--- src/routes/conversations.ts
+++ src/routes/conversations.ts
@@ -1,5 +1,6 @@
 import { zValidator } from '@hono/zod-validator'
 import { and, eq } from 'drizzle-orm'
+import { conversations } from 'src/db/schema/conversations'
 import { HonoVar } from 'src/helpers/hono'
 import { isAuth } from 'src/middlewares/isAuth'
 import { z } from 'zod'
@@ -35,6 +36,40 @@ conversationsRoute.get(
     }
 
     ctx.json(conversation)
+  }
+)
+
+conversationsRoute.post(
+  '/create',
+  isAuth(),
+  zValidator(
+    'json',
+    z.object({
+      title: z.string().min(3),
+      categoryId: z.string(),
+    })
+  ),
+  async (ctx) => {
+    const db = ctx.get('database')
+    const payload = ctx.get('userPayload')
+    const { title, categoryId } = ctx.req.valid('json')
+
+    const conversationList = await db
+      .insert(conversations)
+      .values({
+        title: title,
+        categoryId,
+        userId: payload.id,
+      })
+      .returning()
+
+    if (conversationList.length === 0) {
+      return ctx.json({ message: 'Internal server error' }, 500)
+    }
+
+    const conversation = conversationList[0]
+
+    ctx.json(conversation, 201)
   }
 )
 
