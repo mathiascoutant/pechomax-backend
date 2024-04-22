afb5a5226aa76d81b099e42d93d6c58ef026a519
Rin <dwnssb@gmail.com>
2024-04-22 16:41:22 +0200

    feat: create levels relations

--- src/db/schema/levels.ts
+++ src/db/schema/levels.ts
@@ -1,4 +1,6 @@
+import { relations } from 'drizzle-orm'
 import { integer, pgTable, text, uuid } from 'drizzle-orm/pg-core'
+import { users } from './users'
 
 export const levels = pgTable('levels', {
   id: uuid('id').defaultRandom().primaryKey(),
@@ -7,6 +9,10 @@ export const levels = pgTable('levels', {
   start: integer('start').notNull(),
   score: integer('score').notNull(),
 })
+
+export const levelsRelations = relations(levels, ({ many }) => ({
+  users: many(users),
+}))
 
 export type Level = typeof levels.$inferSelect
 export type NewLevel = typeof levels.$inferInsert
