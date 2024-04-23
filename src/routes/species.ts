57922eb75ed325caee933a724841f59aeceec94f
Rin <dwnssb@gmail.com>
2024-04-23 10:07:16 +0200

    feat: species update routes

--- src/routes/species.ts
+++ src/routes/species.ts
@@ -1,4 +1,5 @@
 import { zValidator } from '@hono/zod-validator'
+import { eq } from 'drizzle-orm'
 import { species } from 'src/db/schema/species'
 import { HonoVar } from 'src/helpers/hono'
 import { isAuth } from 'src/middlewares/isAuth'
@@ -56,6 +57,28 @@ speciesRoute.post(
 
     if (speciesList.length === 0) {
       return ctx.json({ message: 'Failed to create species' }, 500)
+    }
+
+    const speciesItem = speciesList[0]
+
+    return ctx.json(speciesItem)
+  }
+)
+
+speciesRoute.put(
+  '/update/:id',
+  isAuth('Admin'),
+  zValidator('param', z.object({ id: z.string() })),
+  zValidator('json', z.object({ name: z.string().optional(), pointValue: z.number().optional() })),
+  async (ctx) => {
+    const db = ctx.get('database')
+    const { id } = ctx.req.valid('param')
+    const updateSpecies = ctx.req.valid('json')
+
+    const speciesList = await db.update(species).set(updateSpecies).where(eq(species.id, id)).returning()
+
+    if (speciesList.length === 0) {
+      return ctx.json({ message: 'Species not found' }, 404)
     }
 
     const speciesItem = speciesList[0]
