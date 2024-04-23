46ecfb68504129656b497fd5f0153defe97b4bd3
Rin <dwnssb@gmail.com>
2024-04-23 10:27:12 +0200

    feat: speciesLocation update routes

--- src/routes/speciesLocation.ts
+++ src/routes/speciesLocation.ts
@@ -1,4 +1,5 @@
 import { zValidator } from '@hono/zod-validator'
+import { eq } from 'drizzle-orm'
 import { speciesLocation } from 'src/db/schema/speciesLocation'
 import { HonoVar } from 'src/helpers/hono'
 import { isAuth } from 'src/middlewares/isAuth'
@@ -55,6 +56,37 @@ speciesLocationRoute.post(
 
     if (speciesLocationList.length === 0) {
       return ctx.json({ message: 'Failed to create speciesLocation' }, 500)
+    }
+
+    const speciesLocationItem = speciesLocationList[0]
+
+    return ctx.json(speciesLocationItem)
+  }
+)
+
+speciesLocationRoute.put(
+  '/update/:id',
+  zValidator('param', z.object({ id: z.string() })),
+  zValidator(
+    'json',
+    z.object({
+      speciesId: z.string().optional(),
+      locationId: z.string().optional(),
+    })
+  ),
+  async (ctx) => {
+    const db = ctx.get('database')
+    const updateDatas = ctx.req.valid('json')
+    const { id } = ctx.req.valid('param')
+
+    const speciesLocationList = await db
+      .update(speciesLocation)
+      .set(updateDatas)
+      .where(eq(speciesLocation.id, id))
+      .returning()
+
+    if (speciesLocationList.length === 0) {
+      return ctx.json({ message: 'Failed to update speciesLocation' }, 500)
     }
 
     const speciesLocationItem = speciesLocationList[0]
