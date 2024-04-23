04e6a27a48b4b436aaa3a910f9ec7e9fb37d9c15
Rin <dwnssb@gmail.com>
2024-04-23 10:23:36 +0200

    feat: speciesLocation get routes

--- src/routes/speciesLocation.ts
+++ src/routes/speciesLocation.ts
@@ -1,6 +1,40 @@
+import { zValidator } from '@hono/zod-validator'
 import { HonoVar } from 'src/helpers/hono'
 import { isAuth } from 'src/middlewares/isAuth'
+import { z } from 'zod'
 
 const speciesLocationRoute = new HonoVar().basePath('speciesLocation').use(isAuth('Admin'))
+
+speciesLocationRoute.get('/', async (ctx) => {
+  const db = ctx.get('database')
+
+  const speciesLocations = await db.query.speciesLocation.findMany()
+
+  return ctx.json(speciesLocations)
+})
+
+speciesLocationRoute.get(
+  '/:id',
+  zValidator(
+    'param',
+    z.object({
+      id: z.string(),
+    })
+  ),
+  async (ctx) => {
+    const db = ctx.get('database')
+    const { id } = ctx.req.valid('param')
+
+    const speciesLocation = await db.query.speciesLocation.findFirst({
+      where: (speciesLocation, { eq }) => eq(speciesLocation.id, id),
+    })
+
+    if (!speciesLocation) {
+      return ctx.json({ message: 'SpeciesLocation not found' }, 404)
+    }
+
+    return ctx.json(speciesLocation)
+  }
+)
 
 export default speciesLocationRoute
