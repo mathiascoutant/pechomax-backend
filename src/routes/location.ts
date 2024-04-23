d44f05d461b4174a8f3934d891e3155ce3d3ac9b
Rin <dwnssb@gmail.com>
2024-04-23 10:14:08 +0200

    feat: locations create routes

--- src/routes/location.ts
+++ src/routes/location.ts
@@ -1,5 +1,7 @@
 import { zValidator } from '@hono/zod-validator'
+import { locations } from 'src/db/schema/locations'
 import { HonoVar } from 'src/helpers/hono'
+import { isAuth } from 'src/middlewares/isAuth'
 import { z } from 'zod'
 
 const locationsRoute = new HonoVar().basePath('locations')
@@ -33,6 +35,34 @@ locationsRoute.get(
     }
 
     return ctx.json(location)
+  }
+)
+
+locationsRoute.post(
+  '/create',
+  isAuth(),
+  zValidator(
+    'json',
+    z.object({
+      longitude: z.string(),
+      latitude: z.string(),
+      name: z.string(),
+      description: z.string(),
+    })
+  ),
+  async (ctx) => {
+    const db = ctx.get('database')
+    const { longitude, latitude, name, description } = ctx.req.valid('json')
+
+    const locationList = await db.insert(locations).values({ longitude, latitude, name, description }).returning()
+
+    if (locationList.length === 0) {
+      return ctx.json({ message: 'Failed to create location' }, 500)
+    }
+
+    const locationItem = locationList[0]
+
+    return ctx.json(locationItem)
   }
 )
 
