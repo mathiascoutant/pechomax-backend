7ba442c056b49e9be2a13cc6012cc8ad65459c14
Rin <dwnssb@gmail.com>
2024-04-29 09:38:00 +0200

    chore: firebase env

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -9,6 +9,13 @@ const envSchema = z.object({
   COOKIE_SECRET: z.string(),
   JWT_SECRET: z.string(),
   CORS_ORIGIN: z.string(),
+  FIREBASE_API_KEY: z.string(),
+  FIREBASE_AUTH_DOMAIN: z.string(),
+  FIREBASE_PROJECT_ID: z.string(),
+  FIREBASE_STORAGE_BUCKET: z.string(),
+  FIREBASE_MESSAGING_SENDER_ID: z.string(),
+  FIREBASE_APP_ID: z.string(),
+  FIREBASE_MEASUREMENT_ID: z.string(),
   NODE_ENV: z.enum(['DEV', 'PROD']),
 })
 
